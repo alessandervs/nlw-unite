bd6398304a96f25db6f9d212b3e7cbda792b3a13
alessandervs <sanderpant1@hotmail.com>
2024-04-05 20:41:19 -0300

    feat: preparação para ci/cd

--- server-node/prisma/migrations/20240402013151_create_table_events/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "events" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "details" TEXT,
-    "slug" TEXT NOT NULL,
-    "maximum_attendees" INTEGER
-);
